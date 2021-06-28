b86dbd3e3c953d9f6d8f4c6bd929093f48a9b032
rajravi34 <86578363+rajravi34@users.noreply.github.com>
2021-06-27 19:30:24 -0500

    Add files via upload

---
+++ second.py
@@ -0,0 +1,17 @@
+name = "Raj"
+number = 3.1415
+def multiply(a,b,c):
+	return a*b*c
+
+
+
+def add(a,b,c):
+	return a + b + c
+
+
+
+def circumference(radius):
+	return round(2 * number * radius)
+
+
+	
